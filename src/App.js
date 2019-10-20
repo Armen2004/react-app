9d81bd74f40d8448de7c72ba35608307c9d4219e
Armen Hovasapyan <armenhovasapyan@yahoo.com>
2019-10-20 19:26:20 +0400

    added created dynamic component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <h1>Hello</h1>
       <p>This is realy working!!!</p>
-      <Person name="Armen" age="33" />
+      <Person name="Armen" age="33">My Hobbies: Racing</Person>
       <Person name="Armine" age="33" />
     </div>
   );
