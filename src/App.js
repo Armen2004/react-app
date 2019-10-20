04417f6849acb587aaf820c3455393fd39120204
Armen Hovasapyan <armenhovasapyan@yahoo.com>
2019-10-20 20:37:27 +0400

    Update App.js
    using states in component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <p>This is realy working!!!</p>
-      <Person name="Armen" age="33">My Hobbies: Racing</Person>
-      <Person name="Armine" age="33" />
-    </div>
-  );
+class App extends React.Component  {
+  state = {
+    persons: [
+      {name: 'Anna', age: 5},
+      {name: 'Davit', age: 1},
+    ]
+  }
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {name: 'Anna1', age: 5},
+        {name: 'Davit1', age: 1},
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <p>This is realy working!!!</p>
+        <Person name="Armen" age="33">My Hobbies: Racing</Person>
+        <Person name="Armine" age="33" />
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+      </div>
+    );
+  }
 }
 
 export default App;
